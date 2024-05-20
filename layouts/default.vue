2eac91a7f36e10344640dd3efa9ae452c402b65c
SaikaVA <nthanbinh@gmail.com>
2024-05-20 22:53:44 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <UCard>
+    <template #header>
+      <div class="w3-row">
+        <div class="w3-col m3 w3-left-align">Header</div>
+
+        <div class="w3-col m6 w3-center">Wurst</div>
+
+        <div class="w3-col m3 w3-right-align">
+          <ColorModeToggle />
+        </div>
+      </div>
+    </template>
+
+    <slot />
+
+    <template #footer> Footer </template>
+  </UCard>
+</template>
+
+<script setup>
+import ColorModeToggle from "~/components/buttons/ColorModeToggle.vue";
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["~/assets/css/w3.css"],
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+});
